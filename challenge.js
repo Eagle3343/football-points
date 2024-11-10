59a146ce04cdbfeef69ccbc4182bee4642ebd9f1
Eagle3343 <Eagle3343>
2024-11-10 19:49:14 +0400

    code solved

--- challenge.js
+++ challenge.js
@@ -1,2 +1,11 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+
+function CalculatePoints(wins , draws, losses){
+    return (wins*3)+(draws*1)+(losses*0)
+    
+}
+ console.log(CalculatePoints(5,2,3));//17
+ console.log(CalculatePoints(2,1,3));//7
